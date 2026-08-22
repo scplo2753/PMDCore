039cab3022549e6c925f1e6a6b81c5a06355c6bd
scplo2753 <xdsuperisd@gmail.com>
2026-08-22 16:19:09 +0800

    fix(cigar): validate operation length parsing

--- src/parsedData.cpp
+++ src/parsedData.cpp
@@ -1,6 +1,8 @@
 #include "parsedData.hpp"
 #include <algorithm>
 #include <stdexcept>
+#include <charconv>
+#include <limits>
 #include "utilities/sequence_utils.hpp"
 
 static bool isCigarOp(char c)
@@ -174,13 +176,25 @@ std::vector<std::size_t> parsedData::getOpListInCIGAR(char Op)
     std::size_t current_pos = 0;
     for (auto &line : getCIGARList())
     {
-        std::size_t step_long = std::stoi(line.second);
+        std::size_t step_length = 0;
+        const auto [ptr, ec] = std::from_chars(line.second.data(), line.second.data() + line.second.size(), step_length);
+
+        if(ec!=std::errc{}||ptr!=line.second.data() + line.second.size())
+        {
+            throw std::invalid_argument("Invalid CIGAR operation length: " + line.second);
+        }
+
+        if(step_length>std::numeric_limits<std::size_t>::max() - current_pos)
+        {
+            throw std::overflow_error("CIGAR operation length overflow: " + line.second);
+        }
+
         if (line.first == Op)
         {
-            for (std::size_t temp = current_pos; temp<(current_pos + step_long);temp++)
+            for (std::size_t temp = current_pos; temp<(current_pos + step_length);temp++)
                 Op_list.emplace_back(temp);
         }
-        current_pos += std::stoi(line.second);
+        current_pos += step_length;
     }
     return Op_list;
 }

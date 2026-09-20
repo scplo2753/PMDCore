830d4a713b32be8c2adf3af1cc6e730659bc54c7
buwad_likon <584664026@qq.com>
2026-09-20 13:19:04 +0800

    style: format parsedData code

--- include/parsedData.hpp
+++ include/parsedData.hpp
@@ -27,70 +27,154 @@ using CIGARList_t = std::vector<std::pair<char, std::size_t>>;
 
 class parsedData
 {
-public:
+   public:
     // entry function
     [[nodiscard]]
-    static parsedRecordResult parseRawData(const recordLine_struct_t &record);
+    static parsedRecordResult parseRawData(const recordLine_struct_t& record);
 
     // get raw data functions
-    std::string getQNAME() const { return data.QNAME; }
-    std::string getRNAME() const { return data.RNAME; }
-    std::string getRawCigar() const { return data.cigar; }
-    int getRawFLAG() const { return std::stoi(data.FLAG); }
-    std::string getReadSeq() const { return data.read_seq; }
-    int getMAPQ() const { return data.MAPQ; }
-    int getPosition() const { return data.POS; }
-    std::string getQualityScores() const { return data.quality_scores; }
-    const options_map_t& getOptionsMap() const { return data.options_map; }
+    std::string getQNAME() const
+    {
+        return data.QNAME;
+    }
+    std::string getRNAME() const
+    {
+        return data.RNAME;
+    }
+    std::string getRawCigar() const
+    {
+        return data.cigar;
+    }
+    int getRawFLAG() const
+    {
+        return std::stoi(data.FLAG);
+    }
+    std::string getReadSeq() const
+    {
+        return data.read_seq;
+    }
+    int getMAPQ() const
+    {
+        return data.MAPQ;
+    }
+    int getPosition() const
+    {
+        return data.POS;
+    }
+    std::string getQualityScores() const
+    {
+        return data.quality_scores;
+    }
+    const options_map_t& getOptionsMap() const
+    {
+        return data.options_map;
+    }
 
-    //tag functions
-    bool isTagExists(const std::string &tag) const;
-    std::string getTagValue(const std::string &tag) const;
+    // tag functions
+    bool isTagExists(const std::string& tag) const;
+    std::string getTagValue(const std::string& tag) const;
 
     // CIGAR functions
-    CIGARList_t getCIGARList() const {return cigar_list;}
-    std::vector<std::size_t> getInsertionList() { return getOpListInCIGAR('I'); }
-    std::vector<std::size_t> getSoftClipList() { return getOpListInCIGAR('S'); }
-    std::vector<std::size_t> getMatchList() { return getOpListInCIGAR('M'); }
-    std::vector<std::size_t> getDeletionList() { return getOpListInCIGAR('D'); }
+    CIGARList_t getCIGARList() const
+    {
+        return cigar_list;
+    }
+    std::vector<std::size_t> getInsertionList()
+    {
+        return getOpListInCIGAR('I');
+    }
+    std::vector<std::size_t> getSoftClipList()
+    {
+        return getOpListInCIGAR('S');
+    }
+    std::vector<std::size_t> getMatchList()
+    {
+        return getOpListInCIGAR('M');
+    }
+    std::vector<std::size_t> getDeletionList()
+    {
+        return getOpListInCIGAR('D');
+    }
 
     // MD functions
-    std::string getMDTagValue() const { return getTagValue("MD"); }
+    std::string getMDTagValue() const
+    {
+        return getTagValue("MD");
+    }
     std::vector<std::string> getMDList() const;
 
     // status flag functions
     bool isStatusFlagSet(SamFlag flag) const;
-    bool isReadPaired() const { return isStatusFlagSet(SamFlag::READ_PAIRED); }
-    bool isProperPair() const { return isStatusFlagSet(SamFlag::PROPER_PAIR); }
-    bool isReadUnmapped() const { return isStatusFlagSet(SamFlag::READ_UNMAPPED); }
-    bool isMateUnmapped() const { return isStatusFlagSet(SamFlag::MATE_UNMAPPED); }
-    bool isReadReverseStrand() const { return isStatusFlagSet(SamFlag::READ_REVERSE_STRAND); }
-    bool isMateReverseStrand() const { return isStatusFlagSet(SamFlag::MATE_REVERSE_STRAND); }
-    bool isFirstInPair() const { return isStatusFlagSet(SamFlag::FIRST_IN_PAIR); }
-    bool isSecondInPair() const { return isStatusFlagSet(SamFlag::SECOND_IN_PAIR); }
-    bool isNotPrimaryAlignment() const { return isStatusFlagSet(SamFlag::NOT_PRIMARY_ALIGNMENT); }
-    bool isReadFailsQualityChecks() const { return isStatusFlagSet(SamFlag::READ_FAILS_QUALITY_CHECKS); }
-    bool isDuplicateRead() const { return isStatusFlagSet(SamFlag::DUPLICATE_READ); }
-    bool isSupplementaryAlignment() const { return isStatusFlagSet(SamFlag::SUPPLEMENTARY_ALIGNMENT); }
-    bool isReversedSeq() const { return isStatusFlagSet(SamFlag::READ_REVERSE_STRAND); }
+    bool isReadPaired() const
+    {
+        return isStatusFlagSet(SamFlag::READ_PAIRED);
+    }
+    bool isProperPair() const
+    {
+        return isStatusFlagSet(SamFlag::PROPER_PAIR);
+    }
+    bool isReadUnmapped() const
+    {
+        return isStatusFlagSet(SamFlag::READ_UNMAPPED);
+    }
+    bool isMateUnmapped() const
+    {
+        return isStatusFlagSet(SamFlag::MATE_UNMAPPED);
+    }
+    bool isReadReverseStrand() const
+    {
+        return isStatusFlagSet(SamFlag::READ_REVERSE_STRAND);
+    }
+    bool isMateReverseStrand() const
+    {
+        return isStatusFlagSet(SamFlag::MATE_REVERSE_STRAND);
+    }
+    bool isFirstInPair() const
+    {
+        return isStatusFlagSet(SamFlag::FIRST_IN_PAIR);
+    }
+    bool isSecondInPair() const
+    {
+        return isStatusFlagSet(SamFlag::SECOND_IN_PAIR);
+    }
+    bool isNotPrimaryAlignment() const
+    {
+        return isStatusFlagSet(SamFlag::NOT_PRIMARY_ALIGNMENT);
+    }
+    bool isReadFailsQualityChecks() const
+    {
+        return isStatusFlagSet(SamFlag::READ_FAILS_QUALITY_CHECKS);
+    }
+    bool isDuplicateRead() const
+    {
+        return isStatusFlagSet(SamFlag::DUPLICATE_READ);
+    }
+    bool isSupplementaryAlignment() const
+    {
+        return isStatusFlagSet(SamFlag::SUPPLEMENTARY_ALIGNMENT);
+    }
+    bool isReversedSeq() const
+    {
+        return isStatusFlagSet(SamFlag::READ_REVERSE_STRAND);
+    }
 
     // set functions
     void set_ReadSeq_reverseSeq();
 
-private:
+   private:
     // construct function
- parsedData(recordLine_struct_t data, CIGARList_t cigarList)
-     : data(std::move(data)),cigar_list(std::move(cigarList)), flag_isReadReversed(false) {};
+    parsedData(recordLine_struct_t data, CIGARList_t cigarList)
+        : data(std::move(data)), cigar_list(std::move(cigarList)), flag_isReadReversed(false) {};
 
- // functions
- std::vector<std::size_t> getOpListInCIGAR(char Op);
- [[nodiscard]] static parsedRecordError parseCIGAR(const recordLine_struct_t& raw_data, CIGARList_t& output);
+    // functions
+    std::vector<std::size_t> getOpListInCIGAR(char Op);
+    [[nodiscard]] static parsedRecordError parseCIGAR(const recordLine_struct_t& raw_data, CIGARList_t& output);
 
- // factoy functions
-     static bool factory_isTagNotExists(const recordLine_struct_t& data, const std::string& tag);
-     static bool factory_isTagValueEmpty(const recordLine_struct_t& raw_data, const std::string& Tag);
+    // factoy functions
+    static bool factory_isTagNotExists(const recordLine_struct_t& data, const std::string& tag);
+    static bool factory_isTagValueEmpty(const recordLine_struct_t& raw_data, const std::string& Tag);
 
- recordLine_struct_t data;
- CIGARList_t cigar_list;
- bool flag_isReadReversed;
+    recordLine_struct_t data;
+    CIGARList_t cigar_list;
+    bool flag_isReadReversed;
 };
--- src/parsedData.cpp
+++ src/parsedData.cpp
@@ -36,24 +36,24 @@ static bool isCigarOp(char c)
 parsedRecordResult parsedData::parseRawData(const recordLine_struct_t& raw_data)
 {
     CIGARList_t cigar_list{};
-    auto parse_status = parseCIGAR(raw_data,cigar_list);
-    if(parse_status!=parsedRecordError::SUCCESS)
+    auto parse_status = parseCIGAR(raw_data, cigar_list);
+    if (parse_status != parsedRecordError::SUCCESS)
     {
         return parse_status;
     }
 
     if (factory_isTagNotExists(raw_data, "MD"))
     {
-        std::cerr<<"Warning: "<<raw_data.QNAME<<" has no MD tag, this line will be skipped.\n";
+        std::cerr << "Warning: " << raw_data.QNAME << " has no MD tag, this line will be skipped.\n";
         return parsedRecordError::MD_TAG_NOT_EXIST;
     }
     else if (factory_isTagValueEmpty(raw_data, "MD"))
     {
-        std::cerr<<"Warning: "<<raw_data.QNAME<<" has an empty MD tag, this line will be skipped.\n";
+        std::cerr << "Warning: " << raw_data.QNAME << " has an empty MD tag, this line will be skipped.\n";
         return parsedRecordError::MD_TAG_EMPTY;
     }
 
-    return parsedData(raw_data,std::move(cigar_list));
+    return parsedData(raw_data, std::move(cigar_list));
 }
 
 void parsedData::set_ReadSeq_reverseSeq()
@@ -84,7 +84,7 @@ bool parsedData::isStatusFlagSet(SamFlag flag) const
     }
 }
 
-bool parsedData::factory_isTagNotExists(const recordLine_struct_t &data, const std::string &tag)
+bool parsedData::factory_isTagNotExists(const recordLine_struct_t& data, const std::string& tag)
 {
     return !data.options_map.contains(tag);
 }
@@ -107,53 +107,54 @@ std::string parsedData::getTagValue(const std::string& tag) const
 {
     if (isTagExists(tag))
     {
-        std::string values=data.options_map.at(tag).value;
+        std::string values = data.options_map.at(tag).value;
         return values;
     }
     return "";
 }
 
 /******
-* @brief return splited MD tag, The MD tag is split into numbers and non-numbers, for example, "10A5^C3" will be split into ["10", "A", "5", "^C", "3"].
-* @returns return splitd MD tags if exists, else return empty vector. 
-*******/
+ * @brief return splited MD tag, The MD tag is split into numbers and non-numbers, for example, "10A5^C3" will be split
+ * into ["10", "A", "5", "^C", "3"].
+ * @returns return splitd MD tags if exists, else return empty vector.
+ *******/
 std::vector<std::string> parsedData::getMDList() const
 {
     std::vector<std::string> md_list;
 
     const std::string& md_value = getMDTagValue();
 
-        size_t pos = 0;
-        const size_t md_value_size = md_value.size();
-        while (pos < md_value_size)
+    size_t pos = 0;
+    const size_t md_value_size = md_value.size();
+    while (pos < md_value_size)
+    {
+        char c = md_value[pos];
+        if (c >= '0' && c <= '9')
         {
-            char c = md_value[pos];
-            if (c >= '0' && c <= '9')
-            {
             const auto start = pos;
-                while (pos < md_value_size && md_value[pos] >= '0' && md_value[pos] <= '9')
-                {
-                    ++pos;
-                }
+            while (pos < md_value_size && md_value[pos] >= '0' && md_value[pos] <= '9')
+            {
+                ++pos;
+            }
             md_list.emplace_back(md_value.substr(start, pos - start));
-            }
-            else if (c == '^')
+        }
+        else if (c == '^')
+        {
+            size_t start = pos;
+            ++pos;
+            while (pos < md_value_size && md_value[pos] >= 'A' && md_value[pos] <= 'Z')
             {
-                size_t start = pos;
-                ++pos;
-                while (pos < md_value_size && md_value[pos] >= 'A' && md_value[pos] <= 'Z')
-                {
-                    ++pos;
-                }
-                md_list.emplace_back(std::string(md_value.substr(start, pos - start)));
-            }
-            else if (c >= 'A' && c <= 'Z')
-            {
-                md_list.emplace_back(1,c);
                 ++pos;
             }
-            else
-                ++pos;
+            md_list.emplace_back(std::string(md_value.substr(start, pos - start)));
+        }
+        else if (c >= 'A' && c <= 'Z')
+        {
+            md_list.emplace_back(1, c);
+            ++pos;
+        }
+        else
+            ++pos;
     }
     return md_list;
 }
@@ -162,11 +163,11 @@ parsedRecordError parsedData::parseCIGAR(const recordLine_struct_t& raw_data, CI
 {
     if (raw_data.cigar.empty() || raw_data.cigar == "*")
     {
-        std::cerr <<"Warning: "<<raw_data.QNAME << " has no CIGAR data\n";
+        std::cerr << "Warning: " << raw_data.QNAME << " has no CIGAR data\n";
         return parsedRecordError::CIGAR_EMPTY;
     }
 
-    std::vector<std::pair<char,std::size_t>> temp_cigar_list{};
+    std::vector<std::pair<char, std::size_t>> temp_cigar_list{};
     std::string_view cigar = raw_data.cigar;
     const size_t n = cigar.size();
 
@@ -175,8 +176,7 @@ parsedRecordError parsedData::parseCIGAR(const recordLine_struct_t& raw_data, CI
     while (pos < n)
     {
         size_t num_start = pos;
-        while (pos < n && cigar[pos] >= '0' && cigar[pos] <= '9')
-            ++pos;
+        while (pos < n && cigar[pos] >= '0' && cigar[pos] <= '9') ++pos;
 
         if (pos == num_start)
         {
@@ -199,12 +199,13 @@ parsedRecordError parsedData::parseCIGAR(const recordLine_struct_t& raw_data, CI
             return parsedRecordError::INVALID_CIGAR_OPERATION;
         }
 
-        std::string_view str_cigar_step{cigar.data()+num_start,pos-num_start};
+        std::string_view str_cigar_step{cigar.data() + num_start, pos - num_start};
         std::size_t step{};
         auto [ptr, ec] = std::from_chars(str_cigar_step.data(), str_cigar_step.data() + str_cigar_step.size(), step);
-        if(ec!=std::errc{}||ptr!=str_cigar_step.data() + str_cigar_step.size())
+        if (ec != std::errc{} || ptr != str_cigar_step.data() + str_cigar_step.size())
         {
-            std::cerr << "Warning: " << raw_data.QNAME << " has an invalid CIGAR operation length " << str_cigar_step << "\n";
+            std::cerr << "Warning: " << raw_data.QNAME << " has an invalid CIGAR operation length " << str_cigar_step
+                      << "\n";
             return parsedRecordError::INVALID_CIGAR_STEP;
         }
         temp_cigar_list.emplace_back(op, step);
@@ -215,24 +216,22 @@ parsedRecordError parsedData::parseCIGAR(const recordLine_struct_t& raw_data, CI
     return parsedRecordError::SUCCESS;
 }
 
-
 std::vector<std::size_t> parsedData::getOpListInCIGAR(char Op)
 {
     std::vector<std::size_t> Op_list{};
     std::size_t current_pos = 0;
-    for (auto &line : cigar_list)
+    for (auto& line : cigar_list)
     {
         std::size_t step_length = line.second;
 
-        if(step_length>std::numeric_limits<std::size_t>::max() - current_pos)
+        if (step_length > std::numeric_limits<std::size_t>::max() - current_pos)
         {
             throw std::overflow_error("CIGAR operation length overflow: " + std::to_string(line.second));
         }
 
         if (line.first == Op)
         {
-            for (std::size_t temp = current_pos; temp<(current_pos + step_length);temp++)
-                Op_list.emplace_back(temp);
+            for (std::size_t temp = current_pos; temp < (current_pos + step_length); temp++) Op_list.emplace_back(temp);
         }
         current_pos += step_length;
     }
